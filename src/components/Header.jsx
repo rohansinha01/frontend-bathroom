bb4e1698cdd17ac6776c89027f15f63abaf5f564
rohansinha01 <rohansinha01@gmail.com>
2024-04-01 20:00:40 -0400

    header home button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
 
     return (
-        <div className='App'>
+        <div className='Header'>
           <h1>
             People Be Poopin
           </h1>
+        <Link to='/' >Home</Link>
         </div>
     )
 }
